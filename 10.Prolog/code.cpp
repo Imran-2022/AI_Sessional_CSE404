d54357e12866b66d97f969f74542c9f99bb75f39
Imran-2022 <mdimranulhaque.hstu@gmail.com>
2025-01-31 00:07:31 +0600

    ai/start-coding

---
+++ 10.Prolog/code.cpp
@@ -0,0 +1,11 @@
+/*
+
+a. Define simple facts about relationships (e.g., family relations, food preferences) and write queries to test them.
+b. Create rules to define more complex relationships based on existing facts.
+c. Implement predicates for basic arithmetic operations like addition and multiplication.
+d. Experiment with backtracking and the cut operator (!) to control program flow and improve efficiency.
+e. Write recursive predicates to solve problems: Calculating factorial, and Finding the nth Fibonacci number
+f. Design and implement a basic AI-powered medical diagnosis system using Prolog. The system should identify common illnesses by analyzing symptoms and offer appropriate treatment suggestions.
+g. Create a basic natural language parser in Prolog capable of handling simple declarative sentences with subjects, verbs, and objects
+
+*/
